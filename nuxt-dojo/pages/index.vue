0302d3439899af23e5b8e25e820b04a3d0f575d8
Eduardo Silva <thiago.novak7@gmail.com>
2025-02-11 13:53:16 +0000

    pags oficiais

---
+++ nuxt-dojo/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, velit expedita. Perspiciatis quod quas architecto dolore vel reiciendis harum, ab minus voluptatibus blanditiis? Fugit unde dolorem esse reprehenderit doloremque ex!</p>   
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
